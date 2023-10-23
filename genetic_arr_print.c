0dc5368144e5a8ba438fac5c4a2ed49df26af0a1
jiwO0o <145910511+jiwO0o@users.noreply.github.com>
2023-10-24 01:38:15 +0900

    genetic_arr_print

--- genetic_arr_print.c
+++ genetic_arr_print.c
@@ -1,46 +1,37 @@
 #include <stdio.h>
 
-void print_arr(int *base, int size) {
-	for (int i = 0; i < size; i++)
-		printf("%d ", *(base+i));
-	printf("\n");
-}
+
 
 void print_int(void *elem) {
+	
 	printf("%d ", *(int *)elem);
 }
 
-/* void print_float(void *elem) {
+void print_float(void *elem) {
 	printf("%f ", *(float *)elem);
-} */
+}
 
 void genetic_print_arr(void *base, size_t nmemb, size_t size, void (*print)(void *)) {
-	for (int i = 0; i < memb; i++)
-		print(base+i*size);
+	for (int i = 0; i < nmemb; i++)
+		print(base + i * size);
 		
 	printf("\n");
 }
 
-int main(void) {
+/*int main(void) {
 	
 	int arr[10] = {3, 5, 54, 23, 4, 6, 78, 94, 100, 10};
 	
 	genetic_print_arr(arr, 10, sizeof(int), print_int);
 	
 	return 0;
-}
-
-/* void print_arr(int base[], int size) {
-	for (int i = 0; i < size; i++)
-		printf("%d ", base[i]);
-	printf("\n");
-}
+}*/
 
 int main(void) {
 	
-	int arr[10] = {3, 5, 54, 23, 4, 6, 78, 94, 100, 10};
+	float arr[10] = {3.1, 5.2, 5.4, 2.3, 4.5, 6.8, 7.8, 9.4, 10.2, 1.9};
 	
-	print_arr(arr, 10);
+	genetic_print_arr(arr, 10, sizeof(float), print_float);
 	
 	return 0;
-} 위에랑 같은 의미 */
+}
